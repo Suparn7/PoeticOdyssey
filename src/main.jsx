823fd60c8bdea9cba6185d0dcebd70a3c6953b43
suparn7 <71841109+Suparn7@users.noreply.github.com>
2025-01-17 16:48:00 +0530

    added a basename to royerprovider

--- src/main.jsx
+++ src/main.jsx
@@ -59,7 +59,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <RouterProvider router={router} />
+        <RouterProvider router={router} basename="/PoeticOdyssey" />
       </PersistGate>
     </Provider>
   </StrictMode>
